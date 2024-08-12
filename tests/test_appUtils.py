39a30a6f005e95fc37ee7dc5e0c70aca6356ca3f
Michele Facchinelli <m.facchinelli@imperial.ac.uk>
2024-08-12 14:57:53 +0000

    test(fix): actually use `convertToDatetime`

--- tests/test_appUtils.py
+++ tests/test_appUtils.py
@@ -2,7 +2,7 @@
 
 import pytest
 import typer
-from imap_mag.appUtils import getPacketFromApID
+from imap_mag.appUtils import convertToDatetime, getPacketFromApID
 
 from .testUtils import enableLogging, tidyDataFolders  # noqa: F401
 
@@ -14,4 +14,4 @@ def test_get_packet_from_apid_errors_on_invalid_apid() -> None:
 
 def test_convert_to_datetime_on_invalid_datetime() -> None:
     with pytest.raises(typer.Abort):
-        getPacketFromApID("ABCDEF")
+        convertToDatetime("ABCDEF")
