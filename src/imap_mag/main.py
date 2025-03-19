6350e24497042a5caa64db74b2c0edefe8a5ee2b
mhairifin <m.finlayson@imperial.ac.uk>
2025-03-19 16:38:26 +0000

    Remove test cli commands

--- src/imap_mag/main.py
+++ src/imap_mag/main.py
@@ -1,13 +1,12 @@
 """Main module."""
 
 import logging
-import subprocess
 from pathlib import Path
 from typing import Annotated
 
 import typer
 
-from imap_mag.api import apply, calibrate, process
+from imap_mag.api import calibrate, process
 from imap_mag.api.apiUtils import globalState
 from imap_mag.api.fetch import fetch
 
@@ -19,26 +18,8 @@ def hello(name: str):
     print(f"Hello {name}")
 
 
-@app.command()
-def matlab():
-    subprocess.run(["matlab", "-batch", "helloworld"])
-
-
 app.command()(process.process)
 app.command()(calibrate.calibrate)
-app.command()(apply.apply)
-
-
-@app.command()
-def calibrationdemo(
-    file_to_calibrate: str = typer.Argument(
-        help="The file name of the file to be calibrated",
-    ),
-    output_file: str = typer.Argument(help="The file name of the output file"),
-):
-    subprocess.run(
-        ["matlab", "-batch", f'demo("{file_to_calibrate}", "{output_file}")']
-    )
 
 
 def prepareWorkFile(file, configFile) -> Path | None:
