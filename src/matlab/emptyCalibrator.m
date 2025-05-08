5cf600d038d99a79867b033f547d2adb1ae08be7
mhairifin <m.finlayson@imperial.ac.uk>
2025-05-08 16:34:19 +0000

    Calibrator generates offsets across valid part of file

--- src/matlab/emptyCalibrator.m
+++ src/matlab/emptyCalibrator.m
@@ -8,68 +8,85 @@ function emptyCalibrator(date, sciencefile, calfile, datastore, config)
     end
 
 day_to_process = datetime(date);
+day_before = day_to_process - days(1);
+day_after = day_to_process + days(1);
 
 baseScience = readstruct(sciencefile);
 
 epoch = [baseScience.values.time];
 values = {baseScience.values.value};
 
+all_in_day_idx = epoch > dateshift(day_before, 'end', 'day') & epoch < dateshift(day_after, 'start', 'day');
+
 offset_values = zeros(length(epoch), 3);
 timedelta = zeros(length(epoch), 1);
 quality_flags = zeros(length(epoch), 1);
 quality_bitmask = zeros(length(epoch), 1);
 
-num_vals = length(values);
+subset_offset_values = offset_values(all_in_day_idx, :);
+subset_timedelta = timedelta(all_in_day_idx);
+subset_quality_flags = quality_flags(all_in_day_idx);
+subset_quality_bitmask = quality_bitmask(all_in_day_idx);
+subset_epoch = epoch(all_in_day_idx);
 
-for i = 1:20
-    rand_negatives = truncate(-100 * rand([20 1]),6);
-    rand_positives = truncate(100 * rand([20 1]),6);
-    offset_values(1:20,1) = rand_negatives;
-    offset_values(20:39,1) = rand_positives;
+num_vals = length(subset_epoch);
+chunk_size = floor(num_vals/15);
+
+% Generate random x values
+rand_negatives = truncate(-100 * rand([chunk_size 1]),6);
+rand_positives = truncate(100 * rand([chunk_size 1]),6);
+subset_offset_values(1:chunk_size,1) = rand_negatives;
+subset_offset_values(1*chunk_size:2*chunk_size-1,1) = rand_positives;
+
+% Generate random y values
+rand_negatives = truncate(-100 * rand([chunk_size 1]),6);
+rand_positives = truncate(100 * rand([chunk_size 1]),6);
+subset_offset_values(2*chunk_size:3*chunk_size-1,2) = rand_negatives;
+subset_offset_values(3*chunk_size:4*chunk_size-1,2) = rand_positives;
+
+% Generate random z values
+rand_negatives = truncate(-100 * rand([chunk_size 1]),6);
+rand_positives = truncate(100 * rand([chunk_size 1]), 6);
+subset_offset_values(4*chunk_size:5*chunk_size-1,3) = rand_negatives;
+subset_offset_values(5*chunk_size:6*chunk_size-1,3) = rand_positives;
+
+% Generate some NaN offsets
+subset_offset_values(6*chunk_size:7*chunk_size-1,:) = NaN([chunk_size 3]);
+
+% Generate sets of random offsets
+rand_nums = 200*rand([chunk_size,3]) - 100;
+subset_offset_values(7*chunk_size:8*chunk_size-1,:) = truncate(rand_nums,6);
+
+% Generate random positive and negative timedeltas
+subset_timedelta(8*chunk_size:9*chunk_size-1) = truncate(-1 * rand([chunk_size 1]),3);
+subset_timedelta(9*chunk_size:10*chunk_size-1) = truncate(rand([chunk_size 1]),3);
+
+% Set some quality flags
+subset_quality_flags(10*chunk_size:11*chunk_size-1) = 1;
+
+% Randomly set some quality flags
+subset_quality_flags(11*chunk_size:12*chunk_size-1) = round(rand([chunk_size 1]));
+
+% Set each quality bitmask
+small_chunk = floor(chunk_size/7);
+chunk_start = 12*chunk_size;
+for i = 1:7
+subset_quality_bitmask(chunk_start:chunk_start + small_chunk - 1) = bitshift(1,i);
+chunk_start = chunk_start + small_chunk;
 end
 
-for i = 1:20
-    rand_negatives = truncate(-100 * rand([20 1]),6);
-    rand_positives = truncate(100 * rand([20 1]),6);
-    offset_values(40:59,2) = rand_negatives;
-    offset_values(60:79,2) = rand_positives;
-end
+% Set some random quality bitmasks
+subset_quality_bitmask(chunk_start:chunk_start+chunk_size-1) = round(rand([chunk_size 1])*255);
 
-for i = 1:20
-    rand_negatives = truncate(-100 * rand([20,1]),6);
-    rand_positives = truncate(100 * rand([20,1]), 6);
-    offset_values(80:99,3) = rand_negatives;
-    offset_values(100:119,3) = rand_positives;
-end
-
-offset_values(120:139,:) = NaN([20 3]);
-
-for i = 1:20
-    rand_nums = 200*rand([20,3]) - 100;
-    offset_values(140:159,:) = truncate(rand_nums,6);
-end
-
-timedelta(160:179) = truncate(-1 * rand([20 1]),3);
-timedelta(180:199) = truncate(rand([20 1]),3);
-
-quality_flags(200:209) = 1;
-
-quality_flags(210:219) = round(rand([10 1]));
-
-for i= 1:7
-quality_bitmask(220+(i-1)*5:220+i*5-1) = bitshift(1,i);
-end
-
-quality_bitmask(255) = 11;
-quality_bitmask(256) = 12;
-quality_bitmask(257) = 63;
-quality_bitmask(258) = 27;
-quality_bitmask(259) = 109;
+offset_values(all_in_day_idx, :) = subset_offset_values;
+timedelta(all_in_day_idx) = subset_timedelta;
+quality_flags(all_in_day_idx) = subset_quality_flags;
+quality_bitmask(all_in_day_idx) = subset_quality_bitmask;
 
 Metadata.dependencies = [];
 Metadata.science = {sciencefile};
 Metadata.creation_timestamp = string(datetime("now"), 'yyyy-MM-dd''T''HH:mm:ss');
-Metadata.comment = "This is a zero value calibration file";
+Metadata.comment = "This is a random calibration file";
 generateCalFile(epoch, offset_values, timedelta, quality_flags, quality_bitmask, "noop", Metadata, calfile)
 
 end
