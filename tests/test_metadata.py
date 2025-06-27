5c9cb3e444338e9e56550fe374096b628ff43d00
mhairifin <m.finlayson@imperial.ac.uk>
2025-06-27 17:10:05 +0000

    Expand metadata testing

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from imap_mag.io.StandardSPDFMetadataProvider import StandardSPDFMetadataProvider
 
 
-def test_metadata_provider_returns_correct_values():
+def test_metadata_provider_returns_correct_values_for_standard_l2_file():
     provider = StandardSPDFMetadataProvider(
         mission="imap",
         instrument="mag",
@@ -22,7 +22,7 @@ def test_metadata_provider_returns_correct_values():
     )
 
 
-def test_metadata_recovers_correct_values_from_file():
+def test_metadata_recovers_correct_values_from_standard_l2_file():
     filename = "imap_mag_l2_norm-mago_20251017_v001.cdf"
     provider = StandardSPDFMetadataProvider.from_filename(filename)
 
@@ -39,7 +39,24 @@ def test_metadata_recovers_correct_values_from_file():
     assert provider.get_filename() == filename
 
 
-def test_metadata_recovers_correct_values_from_file_without_level():
+def test_metadata_recovers_correct_values_from_standard_l2_pre_file():
+    filename = "imap_mag_l2-pre_norm-mago_20251017_v001.cdf"
+    provider = StandardSPDFMetadataProvider.from_filename(filename)
+
+    assert provider is not None
+    assert provider.mission == "imap"
+    assert provider.instrument == "mag"
+    assert provider.level == "l2-pre"
+    assert provider.descriptor == "norm-mago"
+    assert provider.content_date == datetime(2025, 10, 17)
+    assert provider.version == 1
+    assert provider.extension == "cdf"
+
+    # Check the generated filename matches the original
+    assert provider.get_filename() == filename
+
+
+def test_metadata_recovers_correct_values_from_ancillary_file_without_level():
     filename = "imap_mag_l2-norm-offsets_20251017_20251017_v001.cdf"
     provider = StandardSPDFMetadataProvider.from_filename(filename)
 
@@ -52,6 +69,24 @@ def test_metadata_recovers_correct_values_from_file_without_level():
     assert provider.version == 1
     assert provider.extension == "cdf"
     assert provider.end_date == datetime(2025, 10, 17)
+
+    # Check the generated filename matches the original
+    assert provider.get_filename() == filename
+
+
+def test_metadata_recovers_correct_values_from_ancillary_file_name_without_level_or_end_date():
+    filename = "imap_mag_l2-rotation_20251017_v001.cdf"
+    provider = StandardSPDFMetadataProvider.from_filename(filename)
+
+    assert provider is not None
+    assert provider.mission == "imap"
+    assert provider.instrument == "mag"
+    assert provider.level is None
+    assert provider.descriptor == "l2-rotation"
+    assert provider.content_date == datetime(2025, 10, 17)
+    assert provider.end_date is None
+    assert provider.version == 1
+    assert provider.extension == "cdf"
 
     # Check the generated filename matches the original
     assert provider.get_filename() == filename
