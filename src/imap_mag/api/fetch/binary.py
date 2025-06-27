de65dfd69dc45327b447e8933de0352bff0c3dd0
Michele Facchinelli <19731497+mfacchinelli@users.noreply.github.com>
2025-06-27 13:22:51 +0100

    Update src/imap_mag/api/fetch/binary.py
    Co-authored-by: Alastair Crabtree <a.crabtree@imperial.ac.uk>

--- src/imap_mag/api/fetch/binary.py
+++ src/imap_mag/api/fetch/binary.py
@@ -63,6 +63,7 @@ def fetch_binary(
         {"fetch_binary": {"api": {"auth_code": auth_code}}} if auth_code else {}
     )
 
+    # authcode is usually defined in the config file but the CLI allows for it to be specified on the command cli with --auth-code or in an ENV var WEBPODA_AUTH_CODE
     app_settings = AppSettings(**settings_overrides)  # type: ignore
     work_folder = app_settings.setup_work_folder_for_command(app_settings.fetch_binary)
     initialiseLoggingForCommand(
