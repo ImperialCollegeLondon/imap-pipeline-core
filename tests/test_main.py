fd3df02b04ac62fb590f4bc54bbf862e175cffed
Michele Facchinelli <m.facchinelli@imperial.ac.uk>
2025-07-09 10:58:45 +0000

    test(fix): update tests based on latest functionality

--- tests/test_main.py
+++ tests/test_main.py
@@ -12,7 +12,7 @@ import pytest
 from typer.testing import CliRunner
 
 from imap_mag.main import app
-from tests.util.miscellaneous import DATASTORE, set_env, tidyDataFolders  # noqa: F401
+from tests.util.miscellaneous import DATASTORE, tidyDataFolders  # noqa: F401
 
 runner = CliRunner()
 
@@ -32,7 +32,7 @@ def test_app_says_hello():
             DATASTORE / "hk/mag/l1/hsk-pw/2025/05/imap_mag_l1_hsk-pw_20250502_v001.csv",
         ),
         (
-            "imap_mag_l0_hsk-pw_20250214_v001.pkts",
+            "imap_mag_l0_hsk-pw_20250214_001.pkts",
             DATASTORE / "hk/mag/l1/hsk-pw/2025/05/imap_mag_l1_hsk-pw_20250502_v001.csv",
         ),
     ],
@@ -42,7 +42,7 @@ def test_process_with_binary_hk_converts_to_csv(binary_file, output_file):
     expectedHeader = "epoch,shcoarse,pus_spare1,pus_version,pus_spare2,pus_stype,pus_ssubtype,hk_strucid,p1v5v,p1v8v,p3v3v,p2v5v,p8v,n8v,icu_temp,p2v4v,p1v5i,p1v8i,p3v3i,p2v5i,p8vi,n8vi,fob_temp,fib_temp,magosatflagx,magosatflagy,magosatflagz,magisatflagx,magisatflagy,magisatflagz,spare1,magorange,magirange,spare2,magitfmisscnt,version,type,sec_hdr_flg,pkt_apid,seq_flgs,src_seq_ctr,pkt_len\n"
     expectedFirstLine = "799424368184000000,483848304,0,1,0,3,25,3,1.52370834,1.82973516,3.3652049479999997,2.54942028,9.735992639,-9.7267671632,19.470153600000003,2.36297684,423.7578925213,18.436028516,116.40531765999998,87.2015252,119.75070000000001,90.32580000000002,19.640128302955475,19.482131117873905,NotSaturated,NotSaturated,NotSaturated,NotSaturated,NotSaturated,NotSaturated,0,RANGE0,RANGE0,0,0,0,0,1,1063,3,0,43\n"
     expectedLastLine = "799437851184000000,483861787,0,1,0,3,25,3,1.52370834,1.82973516,3.3652049479999997,2.54942028,9.555648769,-9.5531674296,26.019506700000022,2.3559926719999997,419.9364473837,31.800489164000002,131.13964636,92.94935734500001,193.83599999999998,154.8802,25.938177593750083,25.628958683022688,NotSaturated,NotSaturated,NotSaturated,NotSaturated,NotSaturated,NotSaturated,0,RANGE3,RANGE3,0,0,0,0,1,1063,3,495,43\n"
-    expectedNumRows = 1335
+    expectedNumRows = 668
 
     # Exercise.
     result = runner.invoke(
@@ -59,27 +59,26 @@ def test_process_with_binary_hk_converts_to_csv(binary_file, output_file):
     print("\n" + str(result.stdout))
 
     # Verify.
-    assert result.exit_code == 0
-    assert Path(output_file).exists()
+    try:
+        assert result.exit_code == 0
+        assert Path(output_file).exists()
 
-    with open(output_file) as f:
-        lines = f.readlines()
-        assert expectedHeader == lines[0]
-        assert expectedFirstLine == lines[1]
-        assert expectedLastLine == lines[-1]
-        assert expectedNumRows == len(lines)
-
-    output_file.unlink()
+        with open(output_file) as f:
+            lines = f.readlines()
+            assert expectedHeader == lines[0]
+            assert expectedFirstLine == lines[1]
+            assert expectedLastLine == lines[-1]
+            assert expectedNumRows == len(lines)
+    finally:
+        output_file.unlink(missing_ok=True)
 
 
 def test_process_error_with_unsupported_file_type():
     # Exercise.
     result = runner.invoke(
         app,
-        [
-            "process",
-            str(Path("tests/data/2025/imap_mag_l1a_norm-mago_20250502_v001.cdf")),
-        ],
+        ["process", "imap_mag_l1a_norm-mago_20250502_v001.cdf"],
+        env={"MAG_DATA_STORE": str(DATASTORE)},
     )
 
     print("\n" + str(result.stdout))
@@ -152,12 +151,12 @@ def test_fetch_binary_downloads_hk_from_webpoda(wiremock_manager, mode):
     # Verify.
     assert result.exit_code == 0
     assert Path(
-        "output/hk/mag/l0/hsk-pw/2025/05/imap_mag_l0_hsk-pw_20250502_v001.pkts"
+        "output/hk/mag/l0/hsk-pw/2025/05/imap_mag_l0_hsk-pw_20250502_001.pkts"
     ).exists()
 
     with (
         open(
-            "output/hk/mag/l0/hsk-pw/2025/05/imap_mag_l0_hsk-pw_20250502_v001.pkts",
+            "output/hk/mag/l0/hsk-pw/2025/05/imap_mag_l0_hsk-pw_20250502_001.pkts",
             "rb",
         ) as output,
         open(binary_file, "rb") as input,
@@ -213,12 +212,12 @@ def test_fetch_binary_downloads_hk_from_webpoda_with_ert(wiremock_manager):
     # Verify.
     assert result.exit_code == 0
     assert Path(
-        "output/hk/mag/l0/hsk-pw/2025/05/imap_mag_l0_hsk-pw_20250502_v001.pkts"
+        "output/hk/mag/l0/hsk-pw/2025/05/imap_mag_l0_hsk-pw_20250502_001.pkts"
     ).exists()
 
     with (
         open(
-            "output/hk/mag/l0/hsk-pw/2025/05/imap_mag_l0_hsk-pw_20250502_v001.pkts",
+            "output/hk/mag/l0/hsk-pw/2025/05/imap_mag_l0_hsk-pw_20250502_001.pkts",
             "rb",
         ) as output,
         open(binary_file, "rb") as input,
