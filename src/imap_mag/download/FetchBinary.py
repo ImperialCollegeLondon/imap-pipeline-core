0fd437c1c234b43a8b69f28c44c6ddb97ccebb70
Michele Facchinelli <m.facchinelli@imperial.ac.uk>
2025-07-09 10:31:07 +0000

    feat: split L0 HK by day

--- src/imap_mag/download/FetchBinary.py
+++ src/imap_mag/download/FetchBinary.py
@@ -1,14 +1,47 @@
 """Program to retrieve and process MAG binary files."""
 
+import io
 import logging
-from datetime import datetime, timedelta
+import os
+from datetime import date, datetime, timedelta
 from pathlib import Path
+
+import ccsdspy
+from ccsdspy.utils import iter_packet_bytes
+from rich.progress import Progress
 
 from imap_mag.client.WebPODA import WebPODA
 from imap_mag.io import HKPathHandler
-from imap_mag.util import HKLevel
+from imap_mag.util import HKLevel, TimeConversion
 
 logger = logging.getLogger(__name__)
+
+
+def split_packets_by_day(binary_file: Path) -> dict[date, bytearray]:
+    packets_by_day: dict[date, bytearray] = dict()
+
+    packet_definition = ccsdspy.FixedLength(
+        [
+            ccsdspy.PacketField(name="SHCOARSE", data_type="uint", bit_length=32),
+        ]
+    )
+
+    size = os.path.getsize(binary_file)
+
+    with Progress(refresh_per_second=1) as progress:
+        task = progress.add_task(f"Splitting {binary_file} by S/C day...", total=size)
+
+        for packet_bytes in iter_packet_bytes(binary_file, include_primary_header=True):
+            progress.update(task, advance=len(packet_bytes))
+
+            packet = packet_definition.load(
+                io.BytesIO(packet_bytes), include_primary_header=True
+            )
+
+            day: list[date] = TimeConversion.convert_met_to_date(packet["SHCOARSE"])
+            packets_by_day.setdefault(day[0], bytearray()).extend(packet_bytes)
+
+    return packets_by_day
 
 
 class FetchBinary:
@@ -52,32 +85,37 @@ class FetchBinary:
             ert=use_ert,
         )
 
-        if file.stat().st_size > 0:
-            logger.info(f"Downloaded file from WebPODA: {file}")
+        if file.stat().st_size == 0:
+            logger.debug(f"Downloaded file {file} is empty and will not be used.")
+            return downloaded
 
-            max_ert: datetime | None = self.__web_poda.get_max_ert(
-                packet=packet,
-                start_date=start_date,
-                end_date=end_date,
-                ert=use_ert,
-            )
-            min_time: datetime | None = self.__web_poda.get_min_sctime(
-                packet=packet,
-                start_date=start_date,
-                end_date=end_date,
-                ert=use_ert,
+        logger.info(f"Downloaded file from WebPODA: {file}")
+
+        max_ert: datetime | None = self.__web_poda.get_max_ert(
+            packet=packet,
+            start_date=start_date,
+            end_date=end_date,
+            ert=use_ert,
+        )
+
+        # Split the binary file by S/C day.
+        packets_by_day: dict[date, bytearray] = split_packets_by_day(file)
+
+        for day, packet_bytes in packets_by_day.items():
+            logger.debug(
+                f"Processing {len(packet_bytes)} bytes for {day.strftime('%Y-%m-%d')}."
             )
 
-            downloaded[file] = HKPathHandler(
+            day_file = file.parent / f"{packet}_{day.strftime('%Y%m%d')}_sclk.bin"
+            with open(day_file, "wb") as f:
+                f.write(packet_bytes)
+
+            downloaded[day_file] = HKPathHandler(
                 level=HKLevel.l0.value,
                 descriptor=HKPathHandler.convert_packet_to_descriptor(packet),
-                content_date=(
-                    min_time.replace(hour=0, minute=0, second=0) if min_time else None
-                ),
+                content_date=datetime.combine(day, datetime.min.time()),
                 ert=max_ert,
                 extension="pkts",
             )
-        else:
-            logger.debug(f"Downloaded file {file} is empty and will not be used.")
 
         return downloaded
