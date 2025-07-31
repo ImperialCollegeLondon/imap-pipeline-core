Fix/timestamp precision bug (#114)
Co-authored-by: Michele Facchinelli <m.facchinelli@imperial.ac.uk>

src/mag_toolkit/calibration/CalibrationDefinitions.py
@@ -1,10 +1,60 @@
 from abc import ABC
-from datetime import datetime
 from enum import Enum
+from typing import Annotated
 
-from pydantic import BaseModel
+import numpy as np
+from pydantic import (
+    BaseModel,
+    BeforeValidator,
+    ConfigDict,
+    PlainSerializer,
+)
 
-CDF_FLOAT_FILLVAL = -1e31  # ISTP compliant FILLVAL for CDF_FLOAT
+
+class CONSTANTS:
+    L2_SKELETON_CDF = "resource/l2_dsrf_skeleton.cdf"
+    OFFSET_SKELETON_CDF = "resource/imap_mag_l2-norm-offsets_20250421_20250421_v001.cdf"
+    CDF_FLOAT_FILLVAL = -1e31  # ISTP compliant FILLVAL for CDF_FLOAT
+
+    class CDF_VARS:
+        EPOCH = "epoch"
+        VECTORS = "vectors"
+        MAGNITUDE = "magnitude"
+        RANGE = "range"
+        OFFSETS = "offsets"
+        TIMEDELTAS = "timedeltas"
+        QUALITY_FLAG = "quality_flag"
+        QUALITY_BITMASK = "quality_bitmask"
+        VALIDITY_START_DATETIME = "validity_start_datetime"
+        VALIDITY_END_DATETIME = "validity_end_datetime"
+
+    class CDF_COORDS:
+        AXIS = "axis"
+        X = "x"
+        Y = "y"
+        Z = "z"
+        DIRECTION = "direction"
+
+    class CDF_ATTRS:
+        GENERATION_DATE = "Generation_date"
+        DATA_VERSION = "Data_version"
+        LOGICAL_FILE_ID = "Logical_file_id"
+        MISSION_GROUP = "Mission_group"
+        IS_MAGO = "is_mago"
+
+    class CSV_VARS:
+        EPOCH = "epoch"
+        X = "x"
+        Y = "y"
+        Z = "z"
+        MAGNITUDE = "magnitude"
+        RANGE = "range"
+        QUALITY_FLAGS = "quality_flags"
+        QUALITY_BITMASK = "quality_bitmask"
+
+
+class ArbitraryTypesAllowedBaseModel(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
 
 
 class CalibrationMethod(str, Enum):
@@ -28,21 +78,37 @@ class Mission(str, Enum):
     IMAP = "IMAP"
 
 
-class CalibrationMetadata(BaseModel):
+def convert_time(value: str) -> np.datetime64:
+    return np.datetime64(value, "ns")
+
+
+def serialize_dt(dt: np.datetime64, _info):
+    return np.datetime_as_string(dt)
+
+
+class CalibrationMetadata(ArbitraryTypesAllowedBaseModel):
     dependencies: list[str]
     science: list[str]
-    creation_timestamp: datetime
+    creation_timestamp: Annotated[
+        np.datetime64,
+        BeforeValidator(convert_time),
+        PlainSerializer(serialize_dt, return_type=str),
+    ]
     comment: str | None = None
 
 
-class Value(BaseModel, ABC):
-    time: datetime
+class Value(ArbitraryTypesAllowedBaseModel, ABC):
+    time: Annotated[
+        np.datetime64,
+        BeforeValidator(convert_time),
+        PlainSerializer(serialize_dt, return_type=str),
+    ]
     value: list[float]
     magnitude: float | None = None
 
 
 class CalibrationValue(Value):
-    timedelta: float = 0
+    timedelta: float = 0.0
     quality_flag: int = 0
     quality_bitmask: int = 0
 
@@ -53,6 +119,14 @@ class ScienceValue(Value):
     quality_bitmask: int | None = 0
 
 
-class Validity(BaseModel):
-    start: datetime
-    end: datetime
+class Validity(ArbitraryTypesAllowedBaseModel):
+    start: Annotated[
+        np.datetime64,
+        BeforeValidator(convert_time),
+        PlainSerializer(serialize_dt, return_type=str),
+    ]
+    end: Annotated[
+        np.datetime64,
+        BeforeValidator(convert_time),
+        PlainSerializer(serialize_dt, return_type=str),
+    ]

tests/test_calibration.py
@@ -30,9 +30,9 @@ def prepare_test_file(test_file, sub_folders, year=None, month=None, rename=None
     dest_filename = test_file if rename is None else rename
     if year and month:
         dest_filepath = (
-            Path("output") / sub_folders / str(year) / str(month) / dest_filename
+            Path("output") / sub_folders / str(year) / f"{month:02d}" / dest_filename
         )
-        original_filepath = DATASTORE / f"{sub_folders}/{year}/{month}/{test_file}"
+        original_filepath = DATASTORE / f"{sub_folders}/{year}/{month:02d}/{test_file}"
     else:
         dest_filepath = Path("output") / sub_folders / dest_filename
         original_filepath = DATASTORE / sub_folders / test_file
@@ -43,7 +43,7 @@ def prepare_test_file(test_file, sub_folders, year=None, month=None, rename=None
     )
 
 
-def test_apply_produces_output_science_file_and_offsets_file(tmp_path):
+def test_apply_produces_output_science_file_and_offsets_file_with_data(tmp_path):
     prepare_test_file(
         "imap_mag_l1c_norm-mago_20251017_v001.cdf",
         "science/mag/l1c",
@@ -63,13 +63,26 @@ def test_apply_produces_output_science_file_and_offsets_file(tmp_path):
         input="imap_mag_l1c_norm-mago_20251017_v001.cdf",
         date=datetime(2025, 10, 17),
     )
-
-    assert Path(
+    output_l2_file = (
         "output/science/mag/l2-pre/2025/10/imap_mag_l2-pre_norm-mago_20251017_v000.cdf"
-    ).exists()
-    assert Path(
-        "output/science-ancillary/l2-offsets/2025/10/imap_mag_l2-norm-offsets_20251017_20251017_v000.cdf"
-    ).exists()
+    )
+    assert Path(output_l2_file).exists()
+    output_offsets_file = "output/science-ancillary/l2-offsets/2025/10/imap_mag_l2-norm-offsets_20251017_20251017_v000.cdf"
+    assert Path(output_offsets_file).exists()
+
+    with pycdf.CDF(output_offsets_file) as offsets_cdf:
+        assert "offsets" in offsets_cdf
+        assert "epoch" in offsets_cdf
+        assert "timedeltas" in offsets_cdf
+        assert "quality_flag" in offsets_cdf
+        assert "quality_bitmask" in offsets_cdf
+
+    with pycdf.CDF(output_l2_file) as cdf:
+        assert "vectors" in cdf
+        assert "epoch" in cdf
+        assert "magnitude" in cdf
+        assert "quality_flags" in cdf
+        assert "quality_bitmask" in cdf
 
 
 def test_apply_fails_when_timestamps_dont_align(tmp_path):
@@ -264,43 +277,43 @@ def test_calibration_layer_is_created_with_offsets_for_every_vector(
     matlab_test_setup, tmp_path
 ):
     prepare_test_file(
-        "imap_mag_l1c_norm-mago-four-vectors-four-ranges_20251017_v000.cdf",
+        "imap_mag_l1c_norm-mago-hundred-vectors_20250421_v001.cdf",
         "science/mag/l1c",
         2025,
-        10,
-        rename="imap_mag_l1c_norm-mago_20251017_v000.cdf",
+        4,
+        rename="imap_mag_l1c_norm-mago_20250421_v001.cdf",
     )
 
     calibrate(
-        date=datetime(2025, 10, 17),
+        date=datetime(2025, 4, 21),
         sensor=Sensor.MAGO,
         mode=ScienceMode.Normal,
         method=CalibrationMethod.NOOP,
     )
     assert Path(
-        "output/calibration/layers/2025/10/imap_mag_noop-layer_20251017_v000.json"
+        "output/calibration/layers/2025/04/imap_mag_noop-layer_20250421_v001.json"
     ).exists()
     with open(
-        "output/calibration/layers/2025/10/imap_mag_noop-layer_20251017_v000.json"
+        "output/calibration/layers/2025/04/imap_mag_noop-layer_20250421_v001.json"
     ) as f:
         noop_layer = json.load(f)
 
     assert noop_layer["method"] == "noop"
-    assert len(noop_layer["values"]) == 4
+    assert len(noop_layer["values"]) == 100
 
-    format = "%Y-%m-%dT%H:%M:%S.%f"
     real_timestamps = [
-        "2025-10-17T02:11:51.521309",
-        "2025-10-17T02:11:52.021309",
-        "2025-10-17T02:11:52.521309",
-        "2025-10-17T02:11:53.021309",
+        "2025-04-21T12:16:05.569359872",
+        "2025-04-21T12:16:06.069359872",
+        "2025-04-21T12:16:06.569359872",
+        "2025-04-21T12:16:07.069359872",
     ]
     for val, timestamp in zip(noop_layer["values"], real_timestamps):
-        assert datetime.strptime(val["time"], format) == datetime.strptime(
-            timestamp, format
-        )
+        assert np.datetime64(val["time"]) == np.datetime64(timestamp)
         offset = val["value"]
         assert len(offset) == 3
         assert offset[0] == 0
         assert offset[1] == 0
         assert offset[2] == 0
+        assert val["timedelta"] is not None
+        assert val["quality_flag"] is not None
+        assert val["quality_bitmask"] is not None