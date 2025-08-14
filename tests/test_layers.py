3947c51cc09c19b4fd478ad2fdd75283f52481d8
Michele Facchinelli <m.facchinelli@imperial.ac.uk>
2025-08-14 12:58:15 +0000

    test: add tests for calibration layer loading from CSV

--- tests/test_layers.py
+++ tests/test_layers.py
@@ -3,10 +3,13 @@ from pathlib import Path
 
 import numpy as np
 import pandas as pd
+import pytest
 from spacepy import pycdf
 
 from mag_toolkit.calibration import (
+    CalibrationLayer,
     CalibrationMetadata,
+    CalibrationMethod,
     Mission,
     ScienceLayer,
     ScienceValue,
@@ -133,3 +136,48 @@ def test_science_layer_writes_to_csv(tmp_path):
     assert df.z.iloc[0] == science_layer.values[0].value[2]
     assert df.magnitude.iloc[0] == np.linalg.norm(science_layer.values[0].value)
     assert df.time.iloc[0] == science_layer.values[0].time
+
+
+def test_calibration_layer_loads_csv_correctly():
+    # Set up.
+    calibration_layer = (
+        DATASTORE
+        / "calibration/layers/2025/10/imap_mag_noop-layer-data_20251017_v001.csv"
+    )
+
+    # Exercise.
+    cl = CalibrationLayer.from_file(calibration_layer)
+
+    # Verify metadata.
+    assert cl.mission == Mission.IMAP
+    assert cl.validity.start == np.datetime64("2025-10-17T02:11:51.521309000", "ns")
+    assert cl.validity.end == np.datetime64("2025-10-17T02:11:59.021309000", "ns")
+    assert cl.sensor == Sensor.MAGO
+    assert cl.version == 0
+    assert cl.metadata.dependencies == []
+    assert cl.metadata.science == []
+    assert cl.metadata.data_filename == calibration_layer
+    assert cl.metadata.creation_timestamp is not None
+    assert cl.value_type == ValueType.VECTOR
+    assert cl.method == CalibrationMethod.NOOP
+
+    # Verify values.
+    assert len(cl.values) == 16
+    assert cl.values[0].time == np.datetime64("2025-10-17T02:11:51.521309000", "ns")
+    assert cl.values[-1].time == np.datetime64("2025-10-17T02:11:59.021309000", "ns")
+
+
+def test_calibration_layer_error_on_loading_empty_csv(tmp_path):
+    # Set up.
+    empty_csv = tmp_path / "empty.csv"
+    empty_csv.touch()
+
+    empty_csv.write_text(
+        "time,offset_x,offset_y,offset_z,timedelta,quality_flag,quality_bitmask\n"
+    )
+
+    # Exercise and verify.
+    with pytest.raises(
+        ValueError, match="CSV file is empty or does not contain valid data"
+    ):
+        CalibrationLayer._from_csv(empty_csv)
