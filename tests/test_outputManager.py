c02d86f4098da8a1c6226bea4f9366e43782f5f4
Alastair Crabtree <a.crabtree@imperial.ac.uk>
2025-08-22 16:32:53 +0000

    chore: yet more test stuff

--- tests/test_outputManager.py
+++ tests/test_outputManager.py
@@ -12,11 +12,11 @@ from tests.util.miscellaneous import (
 )
 
 
-def test_copy_new_file(capture_cli_logs, preclean_work_and_output):
+def test_copy_new_file(capture_cli_logs, temp_folder_path):
     # Set up.
-    manager = OutputManager(Path("output"))
+    manager = OutputManager(temp_folder_path)
 
-    original_file = create_test_file(Path(".work/some_test_file.txt"))
+    original_file = create_test_file(Path(f"{temp_folder_path}/some_test_file.txt"))
 
     # Exercise.
     manager.add_file(
@@ -30,22 +30,26 @@ def test_copy_new_file(capture_cli_logs, preclean_work_and_output):
 
     # Verify.
     assert (
-        f"Copied to {Path('output/hk/mag/l1/pwr/2025/05/imap_mag_l1_pwr_20250502_v001.txt')}."
+        f"Copied to {Path(f'{temp_folder_path}/hk/mag/l1/pwr/2025/05/imap_mag_l1_pwr_20250502_v001.txt')}."
         in capture_cli_logs.text
     )
 
     assert Path(
-        "output/hk/mag/l1/pwr/2025/05/imap_mag_l1_pwr_20250502_v001.txt"
+        f"{temp_folder_path}/hk/mag/l1/pwr/2025/05/imap_mag_l1_pwr_20250502_v001.txt"
     ).exists()
 
 
-def test_copy_file_same_content(capture_cli_logs, preclean_work_and_output):
+def test_copy_file_same_content(capture_cli_logs, temp_folder_path):
     # Set up.
-    manager = OutputManager(Path("output"))
+    manager = OutputManager(temp_folder_path)
 
-    original_file = create_test_file(Path(".work/some_test_file.txt"), "some content")
+    original_file = create_test_file(
+        Path(f"{temp_folder_path}/test_copy_file_same_content.txt"), "some content"
+    )
     existing_file = create_test_file(
-        Path("output/hk/mag/l1/pwr/2025/05/imap_mag_l1_pwr_20250502_v001.txt"),
+        Path(
+            f"{temp_folder_path}/hk/mag/l1/pwr/2025/05/imap_mag_l1_pwr_20250502_v001.txt"
+        ),
         "some content",
     )
 
@@ -63,29 +67,38 @@ def test_copy_file_same_content(capture_cli_logs, preclean_work_and_output):
 
     # Verify.
     assert (
-        f"File {Path('output/hk/mag/l1/pwr/2025/05/imap_mag_l1_pwr_20250502_v001.txt')} already exists and is the same. Skipping update."
+        f"File {Path(f'{temp_folder_path}/hk/mag/l1/pwr/2025/05/imap_mag_l1_pwr_20250502_v001.txt')} already exists and is the same. Skipping update."
         in capture_cli_logs.text
     )
 
     assert not Path(
-        "output/hk/mag/l1/pwr/2025/05/imap_mag_l1_pwr_20250502_v002.txt"
+        f"{temp_folder_path}/hk/mag/l1/pwr/2025/05/imap_mag_l1_pwr_20250502_v002.txt"
     ).exists()
     assert existing_file.stat().st_mtime == existing_modification_time
 
 
 def test_copy_file_second_existing_file_with_same_content(
-    capture_cli_logs, preclean_work_and_output
+    capture_cli_logs, temp_folder_path
 ):
     # Set up.
-    manager = OutputManager(Path("output"))
+    manager = OutputManager(temp_folder_path)
 
-    original_file = create_test_file(Path(".work/some_test_file.txt"), "some content")
+    original_file = create_test_file(
+        Path(
+            f"{temp_folder_path}/test_copy_file_second_existing_file_with_same_content.txt"
+        ),
+        "some content",
+    )
     existing_file = create_test_file(
-        Path("output/hk/mag/l1/pwr/2025/05/imap_mag_l1_pwr_20250502_v002.txt"),
+        Path(
+            f"{temp_folder_path}/hk/mag/l1/pwr/2025/05/imap_mag_l1_pwr_20250502_v002.txt"
+        ),
         "some content",
     )
     create_test_file(
-        Path("output/hk/mag/l1/pwr/2025/05/imap_mag_l1_pwr_20250502_v001.txt")
+        Path(
+            f"{temp_folder_path}/hk/mag/l1/pwr/2025/05/imap_mag_l1_pwr_20250502_v001.txt"
+        )
     )
 
     existing_modification_time = existing_file.stat().st_mtime
@@ -102,30 +115,35 @@ def test_copy_file_second_existing_file_with_same_content(
 
     # Verify.
     assert (
-        f"File {Path('output/hk/mag/l1/pwr/2025/05/imap_mag_l1_pwr_20250502_v001.txt')} already exists and is different. Increasing version to 2."
+        f"File {Path(f'{temp_folder_path}/hk/mag/l1/pwr/2025/05/imap_mag_l1_pwr_20250502_v001.txt')} already exists and is different. Increasing version to 2."
         in capture_cli_logs.text
     )
     assert (
-        f"File {Path('output/hk/mag/l1/pwr/2025/05/imap_mag_l1_pwr_20250502_v002.txt')} already exists and is the same. Skipping update."
+        f"File {Path(f'{temp_folder_path}/hk/mag/l1/pwr/2025/05/imap_mag_l1_pwr_20250502_v002.txt')} already exists and is the same. Skipping update."
         in capture_cli_logs.text
     )
 
     assert not Path(
-        "output/hk/mag/l1/pwr/2025/05/imap_mag_l1_pwr_20250502_v003.txt"
+        f"{temp_folder_path}/hk/mag/l1/pwr/2025/05/imap_mag_l1_pwr_20250502_v003.txt"
     ).exists()
     assert existing_file.stat().st_mtime == existing_modification_time
 
 
-def test_copy_file_existing_versions(capture_cli_logs, preclean_work_and_output):
+def test_copy_file_existing_versions(
+    capture_cli_logs,
+    temp_folder_path,
+):
     # Set up.
-    manager = OutputManager(Path("output"))
+    manager = OutputManager(temp_folder_path)
 
-    original_file = create_test_file(Path(".work/some_test_file.txt"), "some content")
+    original_file = create_test_file(
+        Path(f"{temp_folder_path}/test_copy_file_existing_versions.txt"), "some content"
+    )
 
     for version in range(1, 3):
         create_test_file(
             Path(
-                f"output/hk/mag/l1/pwr/2025/05/imap_mag_l1_pwr_20250502_v{version:03}.txt"
+                f"{temp_folder_path}/hk/mag/l1/pwr/2025/05/imap_mag_l1_pwr_20250502_v{version:03}.txt"
             )
         )
 
@@ -142,20 +160,22 @@ def test_copy_file_existing_versions(capture_cli_logs, preclean_work_and_output)
     # Verify.
     for version in range(1, 3):
         assert (
-            f"File {Path(f'output/hk/mag/l1/pwr/2025/05/imap_mag_l1_pwr_20250502_v{version:03}.txt')} already exists and is different. Increasing version to {version + 1}."
+            f"File {Path(f'{temp_folder_path}/hk/mag/l1/pwr/2025/05/imap_mag_l1_pwr_20250502_v{version:03}.txt')} already exists and is different. Increasing version to {version + 1}."
             in capture_cli_logs.text
         )
 
     assert Path(
-        "output/hk/mag/l1/pwr/2025/05/imap_mag_l1_pwr_20250502_v002.txt"
+        f"{temp_folder_path}/hk/mag/l1/pwr/2025/05/imap_mag_l1_pwr_20250502_v002.txt"
     ).exists()
 
 
-def test_copy_file_forced_version(preclean_work_and_output):
+def test_copy_file_forced_version(temp_folder_path):
     # Set up.
-    manager = OutputManager(Path("output"))
+    manager = OutputManager(temp_folder_path)
 
-    original_file = create_test_file(Path(".work/some_test_file.txt"))
+    original_file = create_test_file(
+        Path(f"{temp_folder_path}/test_copy_file_forced_version.txt")
+    )
 
     # Exercise.
     manager.add_file(
@@ -170,5 +190,5 @@ def test_copy_file_forced_version(preclean_work_and_output):
 
     # Verify.
     assert Path(
-        "output/hk/mag/l1/pwr/2025/05/imap_mag_l1_pwr_20250502_v003.txt"
+        f"{temp_folder_path}/hk/mag/l1/pwr/2025/05/imap_mag_l1_pwr_20250502_v003.txt"
     ).exists()
