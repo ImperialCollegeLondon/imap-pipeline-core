2804716d1fe79d624820a12ca1c97224a1578dde
Michele Facchinelli <m.facchinelli@imperial.ac.uk>
2025-10-15 08:51:40 +0000

    fix: force UTC timezone

--- src/imap_mag/util/DatetimeProvider.py
+++ src/imap_mag/util/DatetimeProvider.py
@@ -1,5 +1,5 @@
 import typing
-from datetime import date, datetime, timedelta
+from datetime import date, datetime, timedelta, timezone
 
 T = typing.TypeVar("T", date, datetime)
 
@@ -9,14 +9,16 @@ class DatetimeProvider:
 
     @staticmethod
     def now() -> datetime:
-        return datetime.now()
+        return datetime.now(timezone.utc).replace(tzinfo=None)
 
     @staticmethod
     def today(date_type: type[T] = datetime) -> T:
         today = date_type.today()
 
         if isinstance(today, datetime):
-            return today.replace(hour=0, minute=0, second=0, microsecond=0)
+            return today.astimezone(timezone.utc).replace(
+                hour=0, minute=0, second=0, microsecond=0, tzinfo=None
+            )
         else:
             return today
 
@@ -28,19 +30,17 @@ class DatetimeProvider:
     def yesterday(cls, date_type: type[T] = datetime) -> T:
         return cls.today(date_type) - timedelta(days=1)
 
-    @staticmethod
-    def start_of_hour() -> datetime:
-        return datetime.now().replace(minute=0, second=0, microsecond=0)
+    @classmethod
+    def start_of_hour(cls) -> datetime:
+        return cls.now().replace(minute=0, second=0, microsecond=0)
 
-    @staticmethod
-    def end_of_hour() -> datetime:
-        return datetime.now().replace(minute=59, second=59, microsecond=999999)
+    @classmethod
+    def end_of_hour(cls) -> datetime:
+        return cls.now().replace(minute=59, second=59, microsecond=999999)
 
-    @staticmethod
-    def end_of_today() -> datetime:
-        return datetime.today().replace(
-            hour=23, minute=59, second=59, microsecond=999999
-        )
+    @classmethod
+    def end_of_today(cls) -> datetime:
+        return cls.today().replace(hour=23, minute=59, second=59, microsecond=999999)
 
     @classmethod
     def beginning_of_imap(cls, date_type: type[T] = datetime) -> T:
