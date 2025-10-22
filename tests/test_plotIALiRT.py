a143811c45ca45540033c29f47a2ff525d0533ce
Michele Facchinelli <m.facchinelli@imperial.ac.uk>
2025-10-22 08:55:12 +0000

    test: add test for latest image generation

--- tests/test_plotIALiRT.py
+++ tests/test_plotIALiRT.py
@@ -5,19 +5,29 @@ import tempfile
 from datetime import datetime
 from pathlib import Path
 
+import pytest
 from diffimg import diff
 
 from imap_mag.cli.plot.plot_ialirt import plot_ialirt
-from tests.util.miscellaneous import TEST_DATA, TEST_TRUTH, temp_datastore  # noqa: F401
+from imap_mag.util import DatetimeProvider
+from tests.util.miscellaneous import (
+    TEST_DATA,
+    TEST_TRUTH,
+    temp_datastore,  # noqa: F401
+)
 
 IALIRT_PACKET_DEFINITION = (
     Path(__file__).parent.parent / "src" / "imap_mag" / "packet_def"
 )
 
 
+@pytest.fixture(scope="function", autouse=False)
+def mock_datetime_provider_today_20251021(monkeypatch):
+    monkeypatch.setattr(DatetimeProvider, "today", lambda: datetime(2025, 10, 21))
+
+
 def test_plot_ialirt(
     temp_datastore: Path,  # noqa: F811
-    capture_cli_logs,
 ) -> None:
     # Set up.
     test_data = TEST_DATA / "ialirt_plot_data.csv"
@@ -41,6 +51,7 @@ def test_plot_ialirt(
 
     ((plot_file, path_handler),) = generated_plots.items()
     assert plot_file.exists()
+    assert not (temp_datastore / "quicklook" / "ialirt" / "latest.png").exists()
 
     assert path_handler.content_date == datetime(2025, 10, 21, 8, 36, 23)
 
@@ -57,3 +68,27 @@ def test_plot_ialirt(
 
     assert image_diff is not None
     assert image_diff < 0.0001
+
+
+def test_plot_ialirt_todays_data_copies_to_latest_figure(
+    temp_datastore: Path,  # noqa: F811
+    mock_datetime_provider_today_20251021,
+) -> None:
+    # Set up.
+    test_data = TEST_DATA / "ialirt_plot_data.csv"
+
+    (temp_datastore / "ialirt" / "2025" / "10").mkdir(parents=True, exist_ok=True)
+    shutil.copy(
+        test_data,
+        temp_datastore / "ialirt" / "2025" / "10" / "imap_ialirt_20251021.csv",
+    )
+
+    # Execute.
+    generated_plots = plot_ialirt(
+        start_date=datetime(2025, 10, 21, 0, 0, 0),
+        end_date=datetime(2025, 10, 21, 23, 59, 59),
+    )
+
+    # Verify.
+    assert len(generated_plots) == 1
+    assert (temp_datastore / "quicklook" / "ialirt" / "latest.png").exists()
