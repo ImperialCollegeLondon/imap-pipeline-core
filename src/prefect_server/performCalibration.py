cdae65a5b8388e1ca545ff9bbe649a44b6a7b55f
Michele Facchinelli <m.facchinelli@imperial.ac.uk>
2025-11-07 11:00:19 +0000

    fix: add calibration to database when run in Prefect

--- src/prefect_server/performCalibration.py
+++ src/prefect_server/performCalibration.py
@@ -68,6 +68,7 @@ def gradiometry_flow(
         mode=mode,
         kappa=kappa,
         sc_interference_threshold=sc_interference_threshold,
+        save_mode=SaveMode.LocalAndDatabase,
     )
 
 
@@ -82,13 +83,14 @@ def calibrate_flow(
     method: CalibrationMethod,
     configuration: CalibrationConfig | None,
     sensor: Sensor = Sensor.MAGO,
-):
+) -> Path:
     return calibrate(
         date=date,
         method=method,
         mode=mode,
         sensor=sensor,
         configuration=configuration.model_dump_json() if configuration else None,
+        save_mode=SaveMode.LocalAndDatabase,
     )
 
 
@@ -110,7 +112,7 @@ def calibrate_and_apply_flow(
     Calibrate and apply the calibration in one flow.
     """
     # First, calibrate
-    cal_layer = calibrate_flow(
+    cal_layer: Path = calibrate_flow(
         date=date, method=method, mode=mode, sensor=sensor, configuration=configuration
     )
 
@@ -118,11 +120,10 @@ def calibrate_and_apply_flow(
     science_input = layer.metadata.science[0]
 
     # Then, apply the calibration
-
     apply_flow(
         cal_layer=cal_layer,
-        date=date,
         file=Path(science_input),
+        date=date,
         calibration_output_type=calibration_output_type,
         L2_output_type=L2_output_type,
     )
