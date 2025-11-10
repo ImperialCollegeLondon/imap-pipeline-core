7ee103c8184f5cbc8e0a4a06c9b3906daa4b154e
Michele Facchinelli <m.facchinelli@imperial.ac.uk>
2025-11-10 09:44:36 +0000

    test: add test to make sure all config can be overwritten

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,4 +1,5 @@
 from imap_mag.config import AppSettings, NestedAliasEnvSettingsSource
+from imap_mag.util import Environment
 
 
 def test_custom_env_settings_no_aliases_loads_default():
@@ -121,3 +122,41 @@ def test_custom_env_settings_ignores_empty_nested_aliases():
 
     # Verify.
     assert not settings_overrides
+
+
+def test_override_setting_with_env_var():
+    # Set up.
+    data_store = "/custom/data/store"
+    check_ialirt_work_subfolder = "check_ialirt"
+    fetch_binary_api_url_base = "http://binary-example.com"
+    fetch_binary_api_auth_code = "binary-secret"
+    fetch_science_api_url_base = "http://science-example.com"
+    fetch_science_api_auth_code = "science-secret"
+    upload_root_path = "custom/upload/root/path"
+
+    # Exercise.
+    with Environment(
+        MAG_DATA_STORE=data_store,
+        MAG_CHECK_IALIRT='{"work_sub_folder": "%s"}' % check_ialirt_work_subfolder,  # noqa: UP031
+        MAG_FETCH_BINARY_API_URL_BASE=fetch_binary_api_url_base,
+        MAG_FETCH_BINARY_API_AUTH_CODE=fetch_binary_api_auth_code,
+        MAG_FETCH_SCIENCE_API_URL_BASE=fetch_science_api_url_base,
+        MAG_FETCH_SCIENCE_API_AUTH_CODE=fetch_science_api_auth_code,
+        MAG_UPLOAD='{"root_path": "%s"}' % upload_root_path,  # noqa: UP031
+    ):
+        settings_cls = AppSettings()  # type: ignore
+
+    # Verify.
+    assert str(settings_cls.data_store) == data_store
+    assert settings_cls.check_ialirt.work_sub_folder == check_ialirt_work_subfolder
+    assert settings_cls.fetch_binary.api.url_base == fetch_binary_api_url_base
+    assert (
+        settings_cls.fetch_binary.api.auth_code.get_secret_value()  # type: ignore
+        == fetch_binary_api_auth_code
+    )
+    assert settings_cls.fetch_science.api.url_base == fetch_science_api_url_base
+    assert (
+        settings_cls.fetch_science.api.auth_code.get_secret_value()  # type: ignore
+        == fetch_science_api_auth_code
+    )
+    assert settings_cls.upload.root_path == upload_root_path
