126765f2de98f7559cc8441baf76c6207b71f013
Alastair Crabtree <a.crabtree@imperial.ac.uk>
2026-02-20 09:29:37 +0000

    Increase memory limits for poll_hk deployment

--- src/prefect_server/workflow.py
+++ src/prefect_server/workflow.py
@@ -126,7 +126,7 @@ async def adeploy_flows(local_debug: bool = False):
     poll_hk_deployable = poll_hk_flow.to_deployment(
         name=PREFECT_CONSTANTS.DEPLOYMENT_NAMES.POLL_HK,
         cron=get_cron_from_env(PREFECT_CONSTANTS.ENV_VAR_NAMES.POLL_HK_CRON),
-        job_variables=shared_job_variables | {"mem_limit": "6G", "memswap_limit": "6G"},
+        job_variables=shared_job_variables | {"mem_limit": "8G", "memswap_limit": "10G"},
         tags=[PREFECT_CONSTANTS.PREFECT_TAG],
     )
 
