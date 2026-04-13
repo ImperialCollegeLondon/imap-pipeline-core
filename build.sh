823002309e87372165edc12e745a1a899743437f
Alastair Crabtree <a.crabtree@imperial.ac.uk>
2026-04-13 13:42:28 +0100

    fix: dev dependencies in modern python

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ poetry run python --version
 
 # restore dependencies & create an venv if needed
 echo "Restoring dependencies..."
-poetry install --all-groups -q
+poetry install --all-groups --all-extras
 
 # load the current python virtual environment - assumes you have already probably run "poetry shell" or are calling from build-python-versions.sh
 if [ -d ".venv" ]; then
