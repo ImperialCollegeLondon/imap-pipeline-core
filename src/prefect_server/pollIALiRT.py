5dd42dad3eaf7be2e4ce32ac9709891cddc4f6df
Miruna Serian <mirunaserian@gmail.com>
2026-06-01 11:26:28 +0100

    revert to ialirt dev link and move alerting

--- src/prefect_server/pollIALiRT.py
+++ src/prefect_server/pollIALiRT.py
@@ -1,8 +1,10 @@
 import asyncio
-from datetime import datetime
+from datetime import UTC, datetime, timedelta
 from typing import Annotated
 
+import pytz
 from prefect import flow, task
+from prefect.blocks.notifications import MicrosoftTeamsWebhook
 from prefect.events import Event, emit_event
 from prefect.runtime import flow_run
 from prefect.states import Completed
@@ -23,6 +25,7 @@ from prefect_server.prefectUtils import (
     get_secret_or_env_var,
     try_get_prefect_logger,
 )
+from prefect_server.quicklookIALiRT import quicklook_ialirt_flow
 
 
 def generate_flow_run_name() -> str:
@@ -104,8 +107,54 @@ async def poll_ialirt_flow(
             },
         ),
     ] = AutomaticRunParameters(),
-    timeout_seconds: int = 300,
-    use_database: bool = True,
+    wait_for_new_data_to_arrive: Annotated[
+        bool,
+        Field(
+            json_schema_extra={
+                "title": "Wait for new data to arrive",
+                "description": "If true, the flow will poll for new data until the end date is reached.",
+            }
+        ),
+    ] = True,
+    timeout_seconds: Annotated[
+        int,
+        Field(
+            json_schema_extra={
+                "title": "Timeout",
+                "description": "Time in seconds to wait between polling for new data. Only used when waiting for new data.",
+            }
+        ),
+    ] = 300,
+    plot_last_3_days: Annotated[
+        bool,
+        Field(
+            json_schema_extra={
+                "title": "Plot last 3 days",
+                "description": "If true, the flow will generate a quicklook plot of the downloaded data over the last 3 days.",
+            }
+        ),
+    ] = True,
+    start_date=None,
+    end_date=None,
+    imap_notification_webhook_name: Annotated[
+        str,
+        Field(
+            default=None,
+            json_schema_extra={
+                "title": "IMAP Webhook Name",
+                "description": "Name of the notification webhook to use for IMAP alerts.",
+            },
+        ),
+    ] = PREFECT_CONSTANTS.IMAP_WEBHOOK_BLOCK_NAME,
+    use_database: Annotated[
+        bool,
+        Field(
+            json_schema_extra={
+                "title": "Use Database",
+                "description": "If true, the flow will use the database to track progress.",
+            }
+        ),
+    ] = True,
 ):
     """
     Runs continuously for one hour, sequentially polling the SDC API
@@ -132,39 +181,80 @@ async def poll_ialirt_flow(
     logger.info(f"Starting IALirt Polling: {start_date} - {end_date}")
 
     combined_instruments = VALID_IALIRT_INSTRUMENTS + VALID_IALIRT_HK_INSTRUMENTS
+    logger.info(f"All instruments: {combined_instruments}")
+    if wait_for_new_data_to_arrive:
+        iteration = 1
+        while True:
+            current_time = DatetimeProvider.now()
 
-    iteration = 1
-    while True:
-        current_time = DatetimeProvider.now()
+            if (end_date - current_time).total_seconds() < timeout_seconds:
+                break
 
-        if (end_date - current_time).total_seconds() < timeout_seconds:
-            break
+            logger.info(f"Starting 5-Minute I-ALiRT Polling Batch #{iteration}")
 
-        logger.info(f"Starting 5-Minute I-ALiRT Polling Batch #{iteration}")
+            for inst in combined_instruments:
+                logger.info(f"Polling for {inst.upper()}")
+                try:
+                    await run_ialirt_polling_pipeline_task(
+                        instrument=inst,
+                        run_parameters=run_parameters,
+                        database=database,
+                        settings=settings,
+                    )
+                except Exception as e:
+                    logger.error(f"Download failed for {inst.upper()}: {e}.")
 
+            # Calculate how long the downloads took
+            batch_duration = (DatetimeProvider.now() - current_time).total_seconds()
+
+            # Sleep only for the remainder of the 5 minutes
+            time_to_sleep = max(0, timeout_seconds - batch_duration)
+
+            logger.info(
+                f"Batch #{iteration} complete in {batch_duration:.1f}s. Sleeping for {time_to_sleep:.1f}s"
+            )
+
+            await asyncio.sleep(time_to_sleep)
+            iteration += 1
+    else:
         for inst in combined_instruments:
             logger.info(f"Polling for {inst.upper()}")
-            try:
-                await run_ialirt_polling_pipeline_task(
-                    instrument=inst,
-                    run_parameters=run_parameters,
-                    database=database,
-                    settings=settings,
-                )
-            except Exception as e:
-                logger.error(f"Download failed for {inst.upper()}: {e}.")
+            await run_ialirt_polling_pipeline_task(
+                instrument=inst,
+                run_parameters=run_parameters,
+                database=database,
+                settings=settings,
+            )
 
-        # Calculate how long the downloads took
-        batch_duration = (DatetimeProvider.now() - current_time).total_seconds()
-
-        # Sleep only for the remainder of the 5 minutes
-        time_to_sleep = max(0, timeout_seconds - batch_duration)
-
-        logger.info(
-            f"Batch #{iteration} complete in {batch_duration:.1f}s. Sleeping for {time_to_sleep:.1f}s"
+    if plot_last_3_days:
+        await quicklook_ialirt_flow(
+            start_date=DatetimeProvider.today() - timedelta(days=2),
+            end_date=DatetimeProvider.now(),
+            combined_plot=True,
         )
 
-        await asyncio.sleep(time_to_sleep)
-        iteration += 1
+        # If this is the 6 AM (UK time) polling job, send the latest figure to Teams
+        uk_end_time = end_date.replace(tzinfo=UTC).astimezone(
+            pytz.timezone("Europe/London")
+        )
+
+        if wait_for_new_data_to_arrive and (uk_end_time.hour == 6):
+            imap_webhook_block = await MicrosoftTeamsWebhook.aload(
+                imap_notification_webhook_name
+            )
+
+            latest_ialirt_date = database.get_workflow_progress(  # type: ignore
+                CONSTANTS.DATABASE.IALIRT_PROGRESS_ID
+            )
+            message_body: str = (
+                f"View the [latest I-ALiRT data on Sharepoint]({PREFECT_CONSTANTS.POLL_IALIRT.IALIRT_QUICKLOOK_SHAREPOINT_URL}).\n\n"
+                f"Updated to {latest_ialirt_date.progress_timestamp} UTC."
+            )
+
+            imap_webhook_block.notify_type = "info"
+            await imap_webhook_block.notify(
+                body=message_body,
+                subject="I-ALiRT Latest Quicklook",
+            )  # type: ignore
 
     return Completed(message="Hourly I-ALiRT Polling completed successfully.")
