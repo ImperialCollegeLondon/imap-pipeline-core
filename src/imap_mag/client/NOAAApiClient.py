3c74ca887d6b2d72e3ae40247dcfaeaa3defda6d
Diego Alonso Alvarez <d.alonso-alvarez@imperial.ac.uk>
2026-07-08 14:30:08 +0100

    Split NOAA clieant in two

--- src/imap_mag/client/NOAAApiClient.py
+++ src/imap_mag/client/NOAAApiClient.py
@@ -3,74 +3,95 @@
 import logging
 from datetime import UTC, datetime
 from time import time
-from typing import Any
+from typing import Any, Literal
 
 import requests
 
 logger = logging.getLogger(__name__)
 
 
-class NOAAApiClient:
-    """Interacts with SOLAR-1, ACE and DSCOVR APIs provided by NOAA.
+def _download_json_file(base_url: str, file_name: str) -> list[Any]:
+    """Download a JSON file return its content as a list of objects.
+
+    Args:
+        base_url: The base URL of the API.
+        file_name: The name of the JSON file to download.
+
+    Returns:
+        A list of dictionaries containing the JSON data.
+    """
+
+    url = f"{base_url.rstrip('/')}/{file_name.lstrip('/')}"
+    response = requests.get(url, timeout=30)
+    response.raise_for_status()  # Raise an error for bad responses
+    return response.json()  # Assuming the response is a JSON array
+
+
+class NOAARTSWApiClient:
+    """Interacts with SOLAR-1 and ACE APIs provided by NOAA.
 
     Which specific JSON file to use depends on the spacecraft as well as the
     time range to retrieve, using the file that covers as much as possible
     within the requested time range.
     """
 
-    def __init__(self, solar1_ace_url: str, dscovr_url: str):
-        self._solar1_ace_url = solar1_ace_url
-        self._dscovr_url = dscovr_url
+    def __init__(self, solar1_ace_url: str):
+        self._url = solar1_ace_url
 
-        if not self._solar1_ace_url:
+        if not self._url:
             raise ValueError("SOLAR-1 and ACE URL cannot be empty.")
-        if not self._dscovr_url:
-            raise ValueError("DSCOVR URL cannot be empty.")
 
-    @staticmethod
-    def _download_json_file(base_url: str, file_name: str) -> list[Any]:
-        """Download a JSON file return its content as a list of dictionaries.
-
-        Args:
-            base_url: The base URL of the API.
-            file_name: The name of the JSON file to download.
-
-        Returns:
-            A list of dictionaries containing the JSON data.
-        """
-
-        url = f"{base_url.rstrip('/')}/{file_name.lstrip('/')}"
-        response = requests.get(url, timeout=30)
-        response.raise_for_status()  # Raise an error for bad responses
-        return response.json()  # Assuming the response is a JSON array
-
-    def _get_solar1_ace_data(self) -> list[dict[str, Any]]:
+    def get_data(
+        self, spacecraft: Literal["SOLAR1", "ACE"], content: Literal["mag", "plasma"]
+    ) -> list[dict[str, Any]]:
         """Download SOLAR-1 and ACE data from real-time space weather API.
 
         As there is one file for magnetic field and another for plasma, both containing
-        the last 24h, we can just download both files and return the data. No date range
-        needed.
+        the last 24h, we can just download the selected file and return the data.
+        No date range needed.
 
-        Note that the files might contain data from other spacecrafts as well as
-        variables that are not needed, but we will filter them later in the pipeline.
+        Only the data associated to the selected spacecraft is returned.
+
+        Args:
+            spacecraft: The spacecraft to retrieve the data for. Must be "SOLAR1" or
+                "ACE"
+            content: The content to retrieve. Must be `mag` or `plasma`.
 
         Returns:
             A list of dictionaries containing the combined data from both files.
         """
+        if spacecraft not in ("SOLAR1", "ACE"):
+            raise ValueError(
+                "Invalid spacecraft type requested. "
+                f"It must be 'SOLAR1' or 'ACE', but {spacecraft} found"
+            )
 
-        mag_file = "rtsw_mag_1m.json"
-        plasma_file = "rtsw_plasma_1m.json"
+        if content not in ("mag", "plasma"):
+            raise ValueError(
+                f"Invalid content type requested for {spacecraft}. "
+                f"It must be 'mag' or 'plasma', but {content} found"
+            )
 
-        mag_data: list[dict[str, Any]] = self._download_json_file(
-            self._solar1_ace_url, mag_file
+        _data: list[dict[str, Any]] = _download_json_file(
+            self._url, f"{content}_1m.json"
         )
-        plasma_data: list[dict[str, Any]] = self._download_json_file(
-            self._solar1_ace_url, plasma_file
-        )
 
-        return mag_data + plasma_data
+        # We return only data relevant for the selected spacecraft
+        return [record for record in _data if record["source"] == spacecraft]
 
-    def _get_dscovr_data(self, start_date: datetime) -> list[dict[str, Any]]:
+
+class DSCOVRApiClient:
+    """Interacts with the DSCOVR APIs provided by NOAA."""
+
+    def __init__(self, dscovr_url: str):
+        self._url = dscovr_url
+
+        if not self._url:
+            raise ValueError("DSCOVR URL cannot be empty.")
+
+    def get_data(
+        self, content: Literal["mag", "plasma"], start_date: datetime
+    ) -> list[dict[str, Any]]:
         """Download DSCOVR data from solar wind API.
 
         The DSCOVR API privides several files, each containing data for a specific time
@@ -78,11 +99,24 @@ class NOAAApiClient:
         that covers as much as possible of the requested time range.
 
         Args:
+            content: The content to retrieve. Must be `mag` or `plasma`.
             start_date: The start date of the requested time range.
 
         Returns:
             A list of dictionaries containing the DSCOVR data.
         """
+        if content not in ("mag", "plasma"):
+            raise ValueError(
+                "Invalid content type requested for DSCOVR. "
+                f"It must be 'mag' or 'plasma', but {content} found"
+            )
+
+        start_time = time()
+        logger.info(
+            f"Downloading {content} data for DSCOVR from {start_date} "
+            + f"until {datetime.now(tz=UTC)}."
+        )
+
         # Determine which file to download based on the time range
         time_ranges = {
             "2-hour": 2,
@@ -94,57 +128,22 @@ class NOAAApiClient:
         dt = datetime.now(tz=UTC) - start_date
         for file_name, hours in time_ranges.items():
             if dt.total_seconds() <= hours * 3600:
-                _mag_file = f"mag-{file_name}.json"
-                _plasma_file = f"plasma-{file_name}.json"
+                _file = f"{content}-{file_name}.json"
                 break
         else:
-            _mag_file = "mag-7-day.json"
-            _plasma_file = "plasma-7-day.json"
+            _file = f"{content}-7-day.json"
 
         # Download the data from the DSCOVR API
-        mag_data: list[list[Any]] = self._download_json_file(
-            self._dscovr_url, _mag_file
-        )
-        plasma_data: list[list[Any]] = self._download_json_file(
-            self._dscovr_url, _plasma_file
-        )
+        _data: list[list[Any]] = _download_json_file(self._url, _file)
 
         # Convert the list of lists to a list of dictionaries using the first row as
         # keys
-        mag_cols = mag_data[0]
-        plasma_cols = plasma_data[0]
-        mag_data_dicts = [dict(zip(mag_cols, row)) for row in mag_data[1:]]
-        plasma_data_dicts = [dict(zip(plasma_cols, row)) for row in plasma_data[1:]]
-
-        return mag_data_dicts + plasma_data_dicts
-
-    def get_all_data(
-        self,
-        *,
-        start_date: datetime,
-    ) -> dict[str, list[dict[str, Any]]]:
-        """Download data from all other NOAA spacecrafts.
-
-        Args:
-            start_date: The start date of the requested time range.
-
-        Returns:
-            A list of dictionaries containing the DSCOVR data.
-        """
-        start_time = time()
-        logger.info(
-            f"Downloading data from other L1 spacecrafts from {start_date} "
-            + f"until {datetime.now(tz=UTC)}."
-        )
-
-        solar1_ace_data = self._get_solar1_ace_data()
-        dscovr_data = self._get_dscovr_data(start_date)
+        _cols = _data[0]
+        _data_dicts = [dict(zip(_cols, row)) for row in _data[1:]]
 
         logger.debug(
-            f"Downloaded {len(solar1_ace_data) + len(dscovr_data)} records from other "
-            + f"L1 spacecrafts in {time() - start_time:.2f} seconds."
+            f"Downloaded {len(_data_dicts)} {content} records for DSCOVR in "
+            f"{time() - start_time:.2f} seconds."
         )
-        return {
-            "solar1_ace": solar1_ace_data,
-            "dscovr": dscovr_data,
-        }
+
+        return _data_dicts
